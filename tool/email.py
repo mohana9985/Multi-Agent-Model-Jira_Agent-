9d648b11f73e2686f051dfb53b37a86dc379c1fe
Kureti Mohana Samba Siva <84023294+mohana9985@users.noreply.github.com>
2025-07-26 20:07:19 +0530

    Update email.py

--- tool/email.py
+++ tool/email.py
@@ -1,31 +1,32 @@
+"""
+This module provides functions for sending emails
+"""
+
 import os
-import asyncio
 from email.mime.text import MIMEText
 from aiosmtplib import SMTP
 from langchain_core.tools import tool
 from dotenv import load_dotenv
 
-@tool("individual_email_tool", return_direct=True)
-def individual_email_tool(to_email: list, subject: str, message: str) -> str:
-    """Sends individual email to the list of email address
+
+@tool("email_sender", parse_docstring=True, return_direct=True)
+async def individual_email_tool(to: str, subject: str="test", message: str="test"):
+    """Sends email to an address and uses agent@qt.com as from
 
     Args:
-        to_email (str): list of email address
+        to (str): to email address
         subject (str): subject
         message (str): message of the email
     """
-    for to in to_email:
-       load_dotenv()
-       smtp_host = os.getenv('SMTP_HOST')
-       smtp_port = int(os.getenv('SMTP_PORT'))
-       smtp_username = os.getenv('SMTP_USERNAME01')
-       smtp_password = os.getenv('SMTP_PASSWORD01')
-       email_message = MIMEText(message)
-       email_message["From"] = os.getenv('FROM_EMAIL')
-       email_message["To"] = to
-       email_message["Subject"] = subject
-       with SMTP(hostname=smtp_host, port=smtp_port) as smtp:
-        smtp.login(smtp_username, smtp_password)
-        smtp.send_message(email_message)
-
-
+    load_dotenv()
+    smtp_host = os.getenv('SMTP_HOST')
+    smtp_port = int(os.getenv('SMTP_PORT'))
+    smtp_username = os.getenv('SMTP_USERNAME')
+    smtp_password = os.getenv('SMTP_PASSWORD')
+    email_message = MIMEText(message)
+    email_message["From"] = os.getenv('FROM_EMAIL')
+    email_message["To"] = to
+    email_message["Subject"] = subject
+    async with SMTP(hostname=smtp_host, port=smtp_port) as smtp:
+        await smtp.login(smtp_username, smtp_password)
+        await smtp.send_message(email_message)
